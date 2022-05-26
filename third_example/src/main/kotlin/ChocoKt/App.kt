0ebc570c22723ed54405f15590f21cedb35d0c75
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-05-26 13:44:00 +0200

    feat: integer and real problem

--- third_example/src/main/kotlin/ChocoKt/App.kt
+++ third_example/src/main/kotlin/ChocoKt/App.kt
@@ -1,34 +1,33 @@
 package ChocoKt
 
 import org.chocosolver.solver.Model
-import org.chocosolver.solver.Solution
+
+// Problem with integer and real variables
 
 fun main() {
 
     val model = Model()
 
     // definition of variables
-    val r = model.realVar("R",260.0)
-    val i = model.realVar("I",0.04)
-    val p = model.realVar("P", 1000.0)
-
-    // definition of support variables
-    val b1 = model.realVar("B1", 0.0, 100.0, 0.001)
-    val b2 = model.realVar("B2", 0.0, 100.0, 0.001)
-    val b3 = model.realVar("B3", 0.0, 100.0, 0.001)
-    val b4 = model.realVar("B4", 0.0, 100.0, 0.001)
+    val x = model.intVar("x",1, 10)
+    val y = model.intVar("y", 1, 10)
+    val z = model.realVar("z", 1.0,10.0, 0.001)
 
     // definition of constraints
-    b1.eq(p.mul(i.add(1.0)).sub(r)).equation().post()
-    b2.eq(b1.mul(i.add(1.0)).sub(r)).equation().post()
-    b3.eq(b2.mul(i.add(1.0)).sub(r)).equation().post()
-    b4.eq(b3.mul(i.add(1.0)).sub(r)).equation().post()
+    // x > y
+    x.gt(y).decompose().post()
+    // z > x /\ z < 8
+    z.gt(x.asRealVar()).equation()
+    z.lt(8.0).equation().post()
+
+    // function to maximize
+    model.setObjective(Model.MAXIMIZE,z)
 
     // searching for a feasible solution
     val solver = model.solver
 
     if(solver.solve()){
-        println(b4)
+        println(z)
     }else if(solver.isStopCriterionMet){
         println("The solver could not find a solution nor prove that none exists in the given limits")
     }else {
