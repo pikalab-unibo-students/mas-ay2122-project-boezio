ff8f70f37b58d368a9297a2ba5c912809122e368
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-06-16 09:59:38 +0200

    build(clp-var-creation): structure of the package

--- clp-var-creation/build.gradle.kts
+++ clp-var-creation/build.gradle.kts
@@ -9,9 +9,6 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
-
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
 }
 
 repositories {
@@ -31,11 +28,6 @@ dependencies {
     implementation("it.unibo.tuprolog:parser-theory-jvm:0.20.4")
 
     testImplementation(kotlin("test"))
-}
-
-application {
-    // Define the main class for the application.
-    mainClass.set("ChocoKt.AppKt")
 }
 
 tasks.test {
