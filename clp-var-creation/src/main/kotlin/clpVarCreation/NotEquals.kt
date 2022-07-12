939c6fb9b83295e6526f776556098a880d7fd58d
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-07-12 10:08:10 +0200

    test: relational operators

--- clp-var-creation/src/main/kotlin/clpVarCreation/NotEquals.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/NotEquals.kt
@@ -6,7 +6,7 @@ import it.unibo.tuprolog.solve.primitive.BinaryRelation
 import it.unibo.tuprolog.solve.primitive.Solve
 import org.chocosolver.solver.expression.discrete.arithmetic.ArExpression
 
-object NotEquals: BinaryRelation.NonBacktrackable<ExecutionContext>("#") {
+object NotEquals: BinaryRelation.NonBacktrackable<ExecutionContext>("#\\=") {
     override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term): Solve.Response {
         applyRelConstraint(first, second, ArExpression::ne)
         return replySuccess()
---
+++ clp-var-creation/src/test/kotlin/clpVarCreation/RelationalOpTest.kt
@@ -0,0 +1,223 @@
+package clpVarCreation
+
+import it.unibo.tuprolog.core.Substitution
+import it.unibo.tuprolog.core.parsing.TermParser
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.theory.parsing.ClausesParser
+import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
+import kotlin.test.assertEquals
+
+class RelationalOpTest {
+
+    private val parser = TermParser.withDefaultOperators()
+
+    private val goal = parser.parseStruct(
+        "problem(X,Y),label([X,Y])"
+    )
+
+    @Test
+    fun testGreaterThan() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#>'(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+            // @giuseppeboezio è importante evitare di fare asserzioni facendo il confronto tra stringhe, se possibile.
+            // meglio confrontare strutture dati. questo perchè a seconda di come lanci i test, i numeri dopo gli underscore
+            // nei nomi delle variabili potrebbero cambiare.
+            // usando lo scope del parser come ho fatto qui, puoi recuperare facilmente i riferimenti alle variabili
+            // usate nel goal
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(2),
+                    varOf("Y") to intOf(1)
+                ),
+                solution.substitution
+            )
+        }
+
+        // @giuseppeboezio noterai che il test non passa. La soluzione dà una sostituzione del tipo:
+        // {X_1=10, Y_1=9, X_2=10, Y_2=9}
+        // il che mi fa pensare che ci sono 2 problemi:
+        // 1. il test è over-specifico: per come hai scritto il problema hai davvero garanzia che la prima soluzioe sia X=2 e Y=1?
+        //    (nota che non è nemmeno detto che ci sia lo stesso risultato in tutte le run su tutte le macchine)
+        //    cerca di scrivere un test che passi sempre se l'implementazione è corretta.
+        //    tipo: in realtà, per come hai specificato il problema non ti frega che la X (o la Y) abbia un valore specifico,
+        //     purchè il valore sia un intero e sia tra 1 e 10.
+        // 2. la sostituzione dà 4 variabili al posto di 2, quindi secondo me da qualche parte a livello implementativo gestiamo male le variabili.
+        //    qua tocca debuggare per capire dove.
+    }
+
+    @Test
+    fun testLessThan() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#<'(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(1),
+                    varOf("Y") to intOf(2)
+                ),
+                solution.substitution
+            )
+        }
+
+    }
+
+    @Test
+    fun testGreaterEquals() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#>='(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(1),
+                    varOf("Y") to intOf(1)
+                ),
+                solution.substitution
+            )
+        }
+
+    }
+
+    @Test
+    fun testLessEquals() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#=<'(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(1),
+                    varOf("Y") to intOf(1)
+                ),
+                solution.substitution
+            )
+        }
+
+    }
+
+    @Test
+    fun testEquals() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#='(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(1),
+                    varOf("Y") to intOf(1)
+                ),
+                solution.substitution
+            )
+        }
+    }
+
+    @Test @Ignore
+    fun testNotEquals() {
+
+        val theory = ClausesParser.withDefaultOperators().parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                '#\='(X, Y).
+            """.trimIndent()
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        parser.scope.with {
+
+            assertEquals(
+                Substitution.unifier(
+                    varOf("X") to intOf(1),
+                    varOf("Y") to intOf(2)
+                ),
+                solution.substitution
+            )
+        }
+    }
+}
