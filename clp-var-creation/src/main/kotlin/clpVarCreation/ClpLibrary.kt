d53568805d2a467b5c2a7a9dc4378718a74ccf7f
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-07-14 13:02:12 +0200

    chore: apply DRY to label/1
    - convert label/1 into a rule calling labeling/2

--- clp-var-creation/src/main/kotlin/clpVarCreation/ClpLibrary.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/ClpLibrary.kt
@@ -2,12 +2,22 @@ package clpVarCreation
 
 import it.unibo.tuprolog.solve.library.AliasedLibrary
 import it.unibo.tuprolog.solve.library.Library
+import it.unibo.tuprolog.theory.Theory
 
 object ClpLibrary : AliasedLibrary by Library.aliased(
     alias = "prolog.clp.int",
     primitives = listOf(
-        In, Ins,
-        Label, Labeling,
-        Equals, NotEquals, GreaterThan, GreaterEquals, LessThan, LessEquals
-    ).associate { it.descriptionPair }
+        In,
+        Ins,
+        Labeling,
+        Equals,
+        NotEquals,
+        GreaterThan,
+        GreaterEquals,
+        LessThan,
+        LessEquals
+    ).associate { it.descriptionPair },
+    theory = Theory.of(
+        Label.implementation
+    )
 )
--- clp-var-creation/src/main/kotlin/clpVarCreation/Label.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/Label.kt
@@ -1,27 +1,20 @@
 package clpVarCreation
 
-import it.unibo.tuprolog.core.Substitution
+import it.unibo.tuprolog.core.Scope
 import it.unibo.tuprolog.core.Term
-import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.solve.ExecutionContext
-import it.unibo.tuprolog.solve.primitive.Solve
-import it.unibo.tuprolog.solve.primitive.UnaryPredicate
-import org.chocosolver.solver.variables.Variable
+import it.unibo.tuprolog.solve.rule.RuleWrapper
 
-object Label: UnaryPredicate.NonBacktrackable<ExecutionContext>("label") {
-    override fun Solve.Request<ExecutionContext>.computeOne(first: Term): Solve.Response {
-        ensuringArgumentIsList(0)
-        val logicVariables: List<Var> = (first as it.unibo.tuprolog.core.List).toList().filterIsInstance<Var>().distinct().toList()
-        val chocoModel = chocoModel
-        val configuration = LabelingConfiguration()
-        val chocoToLogic: Map<Variable, Var> = chocoModel.variablesMap(logicVariables)
-        val solver = createChocoSolver(chocoModel, configuration, chocoToLogic)
-        return if (solver.solve()) {
-            replyWith(
-                Substitution.of(chocoToLogic.map { (k, v) -> v to k.valueAsTerm })
-            )
-        } else {
-            replyFail()
-        }
-    }
+/**
+ * label(Vars) :- labeling([], Vars).
+ */
+object Label : RuleWrapper<ExecutionContext>("label", 1) {
+
+    private val Vars by variables
+
+    override val Scope.head: List<Term>
+        get() = ktListOf(Vars)
+
+    override val Scope.body: Term
+        get() = structOf(Labeling.functor, emptyList, Vars)
 }
