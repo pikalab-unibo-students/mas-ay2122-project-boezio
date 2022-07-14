14cdf31b8629c45eb75733711746f5ef94035125
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-07-14 15:52:00 +0200

    chore: improve formatting in Labelling.kt

--- clp-var-creation/src/main/kotlin/clpVarCreation/Labeling.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/Labeling.kt
@@ -8,13 +8,14 @@ import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.primitive.BinaryRelation
 import it.unibo.tuprolog.solve.primitive.Solve
 import org.chocosolver.solver.variables.Variable
+import it.unibo.tuprolog.core.List as LogicList
 
 object Labeling : BinaryRelation.NonBacktrackable<ExecutionContext>("labeling") {
     override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term): Solve.Response {
         ensuringArgumentIsList(0)
-        val keys: Set<Struct> = (first as it.unibo.tuprolog.core.List).toList().filterIsInstance<Struct>().toSet()
+        val keys: Set<Struct> = (first as LogicList).toList().filterIsInstance<Struct>().toSet()
         ensuringArgumentIsList(1)
-        val logicVariables: List<Var> = (second as it.unibo.tuprolog.core.List).toList().filterIsInstance<Var>().distinct().toList()
+        val logicVariables: List<Var> = (second as LogicList).toList().filterIsInstance<Var>().distinct().toList()
         val chocoModel = chocoModel
         val configuration = LabelingConfiguration.fromTerms(keys)
         val chocoToLogic: Map<Variable, Var> = chocoModel.variablesMap(logicVariables)
