b999084fb193c6fd69c761a76f2d61898fc11c90
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-07-19 17:55:43 +0200

    fix: swapping first and second term in scalar_product

--- clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/ScalarProduct.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/ScalarProduct.kt
@@ -4,12 +4,9 @@ import clpVarCreation.*
 import clpVarCreation.chocoModel
 import clpVarCreation.flip
 import clpVarCreation.variablesMap
-import it.unibo.tuprolog.core.Atom
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.solve.ExecutionContext
-import it.unibo.tuprolog.solve.primitive.PrimitiveWrapper.Companion.ensuringArgumentIsAtom
-import it.unibo.tuprolog.solve.primitive.PrimitiveWrapper.Companion.ensuringArgumentIsList
 import it.unibo.tuprolog.core.Integer as LogicInteger
 import it.unibo.tuprolog.solve.primitive.QuaternaryRelation
 import it.unibo.tuprolog.solve.primitive.Solve
@@ -18,10 +15,10 @@ import org.chocosolver.solver.variables.IntVar
 object ScalarProduct : QuaternaryRelation.NonBacktrackable<ExecutionContext>("scalar_product") {
     override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term, third: Term, fourth: Term): Solve.Response {
         ensuringArgumentIsList(0)
-        val listVars = first.castToList().toList().filterIsInstance<Var>().distinct().toSet()
-        ensuringArgumentIsList(1)
         // this predicate is more limited than SWI Prolog one because it accepts only integer coefficients
-        val listCoeffs = second.castToList().toList().filterIsInstance<LogicInteger>().toSet()
+        val listCoeffs = first.castToList().toList().filterIsInstance<LogicInteger>().toSet()
+        ensuringArgumentIsList(1)
+        val listVars = second.castToList().toList().filterIsInstance<Var>().distinct().toSet()
         if (listVars.size != listCoeffs.size)
             throw IllegalStateException()
         val coeffs = listCoeffs.map { it.value as Int } as IntArray
---
+++ clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/ScalarProductTest.kt
@@ -0,0 +1,47 @@
+package clpVarCreation.globalConstraints
+
+import clpVarCreation.ClpFdLibrary
+import clpVarCreation.assertSolutionAssigns
+import it.unibo.tuprolog.core.parsing.TermParser
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.theory.parsing.ClausesParser
+import org.junit.jupiter.api.Test
+
+class ScalarProductTest {
+
+    private val termParser = TermParser.withDefaultOperators()
+    private val theoryParser = ClausesParser.withDefaultOperators()
+
+    @Test
+    fun testScalarProductInteger() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(X, Y) :- 
+                in(X, '..'(1, 10)), 
+                in(Y, '..'(1, 10)), 
+                scalar_product([1, 2], [X,Y], #=, 10).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X,Y),label([X,Y])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("X") to intOf(1),
+                varOf("Y") to intOf(5)
+            )
+        }
+    }
+
+}
