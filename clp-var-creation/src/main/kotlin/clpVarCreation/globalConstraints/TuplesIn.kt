bfed1d23e317508c03866f20f3d494950048d8c5
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-07-19 21:17:39 +0200

    feat: tuples_in global constraint

---
+++ clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/TuplesIn.kt
@@ -0,0 +1,43 @@
+package clpVarCreation.globalConstraints
+
+import clpVarCreation.chocoModel
+import clpVarCreation.flip
+import clpVarCreation.variablesMap
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.solve.ExecutionContext
+import it.unibo.tuprolog.solve.primitive.BinaryRelation
+import it.unibo.tuprolog.solve.primitive.Solve
+import org.chocosolver.solver.constraints.extension.Tuples
+import it.unibo.tuprolog.core.List as LogicList
+import org.chocosolver.solver.variables.IntVar
+import it.unibo.tuprolog.core.Integer as LogicInteger
+
+object TuplesIn : BinaryRelation.NonBacktrackable<ExecutionContext>("tuples_in") {
+    override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term): Solve.Response {
+        ensuringArgumentIsList(0)
+        ensuringArgumentIsList(1)
+        val listVar = first.castToList().toList()
+        require(listVar.let { it.size == 1 && it is LogicList }) {
+            "first argument is invalid"
+        }
+        val innerList = listVar[0].castToList().toList()
+        val logicVars = innerList.filterIsInstance<Var>().distinct()
+        val chocoModel = chocoModel
+        val varsMap = chocoModel.variablesMap(logicVars).flip()
+        val tuple = innerList.filterIsInstance<Var>().map { varsMap[it] as IntVar }.toTypedArray()
+        val relation = Tuples(true)
+        val tuples = second.castToList().toList()
+        val numElemList = innerList.size
+        for (elem in tuples) {
+            require(elem.let { it is LogicList && it.castToList().toList().size == numElemList }) {
+                "$elem is invalid"
+            }
+            val elemRelation = elem.castToList().toList().filterIsInstance<LogicInteger>().map { it.value.toInt() }.toIntArray()
+            relation.add(elemRelation)
+        }
+        return replySuccess {
+            chocoModel.table(tuple, relation, "CT+").post()
+        }
+    }
+}
