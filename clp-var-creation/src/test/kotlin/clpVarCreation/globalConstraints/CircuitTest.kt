1fd0e5939279e831b18d231b3609e194526bdd8d
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-07-20 10:16:45 +0200

    test(circuit): test did not pass

---
+++ clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/CircuitTest.kt
@@ -0,0 +1,46 @@
+package clpVarCreation.globalConstraints
+
+import clpVarCreation.ClpFdLibrary
+import clpVarCreation.assertSolutionAssigns
+import it.unibo.tuprolog.core.parsing.TermParser
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.solve.library.Libraries
+import it.unibo.tuprolog.theory.parsing.ClausesParser
+import org.junit.jupiter.api.Test
+
+class CircuitTest {
+
+    private val termParser = TermParser.withDefaultOperators()
+    private val theoryParser = ClausesParser.withDefaultOperators()
+
+    @Test
+    fun testCircuit() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(X,Y,Z) :- 
+                ins([X,Y,Z], '..'(1, 3)), 
+                circuit([X,Y,Z]).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X,Y,Z),label([X,Y,Z])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("X") to intOf(2),
+                varOf("Y") to intOf(3),
+                varOf("Z") to intOf(1)
+            )
+        }
+    }
+}
