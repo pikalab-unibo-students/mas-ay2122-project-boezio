aa61ca7b8c8eb3a88e4b29b92f3f5b3a3cd762ae
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-08-04 21:05:33 +0200

    feat(global constraints): cumulative one

--- clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/CumulativeOne.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/CumulativeOne.kt
@@ -1,9 +1,18 @@
 package clpVarCreation.globalConstraints
 
+import clpVarCreation.domain.In
+import it.unibo.tuprolog.core.Scope
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.solve.ExecutionContext
+import it.unibo.tuprolog.solve.classic.stdlib.rule.Call
 import it.unibo.tuprolog.solve.rule.RuleWrapper
+import it.unibo.tuprolog.solve.stdlib.primitive.Univ
 
 /**
+ *
+ * Tasks is a list of struct task/5
+ *
  * cumulative(Tasks) :-
  *          L = 1,
  *          cumulative(Tasks, [limit(L)]).
@@ -11,6 +20,16 @@ import it.unibo.tuprolog.solve.rule.RuleWrapper
 
 object CumulativeOne : RuleWrapper<ExecutionContext>("cumulative", 1) {
 
-    // To implement
+    val Tasks by variables
+    val L by variables
+
+    override val Scope.head: List<Term>
+        get() = ktListOf(Tasks)
+
+    override val Scope.body: Term
+        get() = tupleOf(
+            structOf(In.functor, L, intOf(1)),
+            structOf(CumulativeTwo.functor, Tasks, listFrom(structOf("limit", L)))
+        )
 
 }
