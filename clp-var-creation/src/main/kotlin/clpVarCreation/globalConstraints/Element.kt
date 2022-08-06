f0083f7b72007ba13f9fbc8f031abafc02f38378
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-08-06 12:05:10 +0200

    feat(element): predicate supports both variables and integers for arguments index and value

--- clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/Element.kt
+++ clp-var-creation/src/main/kotlin/clpVarCreation/globalConstraints/Element.kt
@@ -1,10 +1,12 @@
 package clpVarCreation.globalConstraints
 
+import clpVarCreation.*
 import clpVarCreation.chocoModel
 import clpVarCreation.flip
 import clpVarCreation.setChocoModel
 import clpVarCreation.variablesMap
 import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.solve.ExecutionContext
 import it.unibo.tuprolog.solve.primitive.Solve
 import it.unibo.tuprolog.solve.primitive.TernaryRelation
@@ -13,9 +15,15 @@ import org.chocosolver.solver.variables.IntVar
 
 object Element : TernaryRelation.NonBacktrackable<ExecutionContext>("element") {
     override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term, third: Term): Solve.Response {
-        ensuringArgumentIsVariable(0)
-        val index = first.castToVar()
+        val logicVars = first.variables.toSet().union(second.variables.toSet()).union(third.variables.toSet())
+        val chocoModel = chocoModel
+        val varsMap = chocoModel.variablesMap(logicVars).flip()
+        require(first.let { it is Var || it is LogicInteger }){
+            "$first is neither a variable nor an integer"
+        }
+        val index = getAsIntVar(first, varsMap)
         ensuringArgumentIsList(1)
+        val intVarTable = mutableListOf<IntVar>()
         val table = second.castToList().toList()
         for (elem in table) {
             require(elem is LogicInteger) {
@@ -23,14 +31,12 @@ object Element : TernaryRelation.NonBacktrackable<ExecutionContext>("element") {
             }
         }
         val integerList = table.filterIsInstance<LogicInteger>().map { it.value.toInt() }.toIntArray()
-        ensuringArgumentIsVariable(2)
-        val value = third.castToVar()
-        val logicVars = setOf(value, index)
-        val chocoModel = chocoModel
-        val varsMap = chocoModel.variablesMap(logicVars).flip()
-        val chocoValue = varsMap[value] as IntVar
-        val chocoIndex = varsMap[index] as IntVar
-        chocoModel.element(chocoValue, integerList, chocoIndex).post()
+
+        require(third.let { it is Var || it is LogicInteger }){
+            "$first is neither a variable nor an integer"
+        }
+        val value = getAsIntVar(third, varsMap)
+        chocoModel.element(value, integerList, index).post()
         return replySuccess {
             setChocoModel(chocoModel)
         }
