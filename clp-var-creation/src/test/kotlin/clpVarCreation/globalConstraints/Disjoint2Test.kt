d2132bc4b932826ddb596dc207da70bed5f40c25
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-08-10 18:39:26 +0200

    test(disjoint2): test limit cases

--- clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/Disjoint2Test.kt
+++ clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/Disjoint2Test.kt
@@ -8,6 +8,7 @@ import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.solve.library.Libraries
 import it.unibo.tuprolog.theory.parsing.ClausesParser
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class Disjoint2Test: BaseTest() {
 
@@ -46,5 +47,61 @@ class Disjoint2Test: BaseTest() {
                 varOf("Y2") to intOf(2),
             )
         }
+    }
+
+    @Test
+    fun testInvalidRectangleStruct() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(X1, Y1, X2, Y2) :- 
+                in(X1, '..'(1, 1)),
+                in(X2, '..'(3, 3)),
+                in(W1, '..'(3, 3)),
+                in(W2, '..'(2, 2)),
+                ins([Y1, Y2], '..'(1, 10)),
+                ins([H2], '..'(1, 6)),
+                disjoint2([f(X1,W1,Y1),g(X2,W2,Y2,H2)]).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X1, Y1, X2, Y2),label([X1,Y1, X2, Y2])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        assertException(solver, goal)
+    }
+
+    @Test
+    fun testInvalidRectangleArgument() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(X1, Y1, X2, Y2) :- 
+                in(X1, '..'(1, 1)),
+                in(X2, '..'(3, 3)),
+                in(W1, '..'(3, 3)),
+                in(W2, '..'(2, 2)),
+                ins([Y1, Y2], '..'(1, 10)),
+                ins([H2], '..'(1, 6)),
+                disjoint2([f(a,W1,Y1),g(X2,W2,Y2,H2)]).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X1, Y1, X2, Y2),label([X1,Y1, X2, Y2])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        assertException(solver, goal)
     }
 }
