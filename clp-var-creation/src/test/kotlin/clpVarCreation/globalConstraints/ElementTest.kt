86135ce553447fb795a57c3e65e426ea60e76114
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-08-11 09:03:44 +0200

    test(element): test of limit cases

--- clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/ElementTest.kt
+++ clp-var-creation/src/test/kotlin/clpVarCreation/globalConstraints/ElementTest.kt
@@ -8,6 +8,8 @@ import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.solve.library.Libraries
 import it.unibo.tuprolog.theory.parsing.ClausesParser
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class ElementTest: BaseTest() {
 
@@ -39,6 +41,81 @@ class ElementTest: BaseTest() {
                 varOf("X") to intOf(2),
                 varOf("Y") to intOf(3)
             )
+        }
+    }
+
+    @Test
+    fun testInvalidIndex() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(Value) :- 
+                in(Value, '..'(1, 10)),
+                element(a, [5,3,8,2,4], Value).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X),label([X])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        assertThrows<IllegalArgumentException> {
+            solver.solveOnce(goal)
+        }
+    }
+
+    @Test
+    fun testInvalidList() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(Value) :- 
+                in(Value, '..'(1, 10)),
+                element(1, [5,3,8,a,4], Value).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X),label([X])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        assertThrows<IllegalArgumentException> {
+            solver.solveOnce(goal)
+        }
+    }
+
+    @Test
+    fun testInvalidValue() {
+
+        val theory = theoryParser.parseTheory(
+            """
+            problem(Value) :- 
+                in(Value, '..'(1, 10)),
+                element(1, [5,3,8,a,4], a).
+            """.trimIndent()
+        )
+
+        val goal = termParser.parseStruct(
+            "problem(X),label([X])"
+        )
+
+        val solver = Solver.prolog.solverOf(
+            staticKb = theory,
+            libraries = Libraries.of(ClpFdLibrary)
+        )
+
+        assertThrows<IllegalArgumentException> {
+            solver.solveOnce(goal)
         }
     }
 }
