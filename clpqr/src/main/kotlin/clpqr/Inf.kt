3d96eb72e6911b0a06c7cbd95f22f5395e56d83a
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-08-17 10:45:15 +0200

    feat: inf predicate

---
+++ clpqr/src/main/kotlin/clpqr/Inf.kt
@@ -0,0 +1,31 @@
+package clpqr
+
+
+import clpCore.chocoModel
+import clpCore.flip
+import clpCore.variablesMap
+import clpqr.search.Configuration
+import clpqr.search.ProblemType
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.solve.ExecutionContext
+import it.unibo.tuprolog.solve.primitive.QuaternaryRelation
+import it.unibo.tuprolog.solve.primitive.Solve
+
+object Inf: QuaternaryRelation.NonBacktrackable<ExecutionContext>("inf") {
+    override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term, third: Term, fourth: Term): Solve.Response {
+        ensuringArgumentIsStruct(0)
+        val expressionVars = first.variables.toList()
+        ensuringArgumentIsVariable(1)
+        val inf = second.castToVar()
+        ensuringArgumentIsList(3)
+        require(third.castToList().args.all { it is Var }){
+            "Vector is not a list of variables"
+        }
+        ensuringArgumentIsVariable(3)
+        val vertex = fourth.castToVar()
+        val varsMap = chocoModel.variablesMap(expressionVars)
+        val config = Configuration(problemType = ProblemType.MINIMIZE, objective = first)
+        return solve(chocoModel, config, varsMap)
+    }
+}
