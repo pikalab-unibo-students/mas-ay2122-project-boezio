3f8877c1f18935a52ecec07b2d46cf75fe09f5bf
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-24 10:52:47 +0200

    test: maximize test when all constraints are declared in the goal

--- clpqr/src/test/kotlin/clpqr/optimization/MaximizeTest.kt
+++ clpqr/src/test/kotlin/clpqr/optimization/MaximizeTest.kt
@@ -6,6 +6,7 @@ import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.solve.flags.FlagStore
 import it.unibo.tuprolog.solve.library.Libraries
 import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
 class MaximizeTest: BaseTest() {
 
@@ -29,6 +30,34 @@ class MaximizeTest: BaseTest() {
             otherLibraries = Libraries.of(ClpQRLibrary),
             flags = FlagStore.DEFAULT + (Precision to Real.of(precision)),
             staticKb = theory
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        val xExpected = "7.0001079596553750"
+        val yExpected = "2.0007557175876030"
+        val zExpected = "309.9924428241242000"
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                precision,
+                varOf("X") to realOf(xExpected),
+                varOf("Y") to realOf(yExpected),
+                varOf("Z") to realOf(zExpected)
+            )
+        }
+    }
+
+    @Test @Ignore
+    fun testMaximizeVariableGoal(){
+
+        val goal = termParser.parseStruct(
+            "{ 2*X+Y =< 16, X+2*Y =< 11, X+3*Y =< 15, Z = 30*X+50*Y }, maximize(Z)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = Libraries.of(ClpQRLibrary),
+            flags = FlagStore.DEFAULT + (Precision to Real.of(precision))
         )
 
         val solution = solver.solveOnce(goal)
