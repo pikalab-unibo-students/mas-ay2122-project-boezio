4fede35fd3c1bdf4ee1e4d6de8d375772f0d5559
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-25 12:08:37 +0200

    build(clpb): new module for clpb constraint library on boolean variables

---
+++ clpb/src/main/kotlin/clpb/ClpBLibrary.kt
@@ -0,0 +1,8 @@
+package clpb
+
+import it.unibo.tuprolog.solve.library.Library
+import it.unibo.tuprolog.theory.Theory
+
+object ClpBLibrary: Library by Library.of(
+    alias = "prolog.clp.boolean"
+)
