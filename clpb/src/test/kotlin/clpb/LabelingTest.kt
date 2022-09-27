a186dc9906c977e383fb318db448beff79be6f20
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-27 16:47:28 +0200

    test(clpb): labeling/1 tested

--- clpb/src/test/kotlin/clpb/LabelingTest.kt
+++ clpb/src/test/kotlin/clpb/LabelingTest.kt
@@ -21,9 +21,29 @@ class LabelingTest: BaseTest() {
 
         termParser.scope.with {
             solution.assertSolutionAssigns(
-                varOf("X") to intOf("1"),
+                varOf("X") to intOf("1")
             )
         }
+    }
 
+    @Test
+    fun testLabelingAnd(){
+
+        val goal = termParser.parseStruct(
+            "sat(X+Y),labeling([X,Y])"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("X") to intOf("0"),
+                varOf("Y") to intOf(1)
+            )
+        }
     }
 }
