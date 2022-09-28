a7a7a4b1aa72baa10e047b1e3a39bf8b3aeca377
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-28 12:53:56 +0200

    test(clpb): labeling/1 tested for limit cases

--- boolean_variables/src/main/kotlin/ChocoKt/App.kt
+++ boolean_variables/src/main/kotlin/ChocoKt/App.kt
@@ -6,13 +6,13 @@ fun main(){
     val model = Model()
 
     val x = model.boolVar("X")
-    val y = model.boolVar("Y")
+    val y = model.boolVar(true)
 
-    model.addClauses(LogOp.and(x,y))
+    model.addClauses(LogOp.or(x,y))
 
     val solver = model.solver
 
-    while(solver.solve()) {
-        print("${x.booleanValue}\n${y.booleanValue}")
+    if(solver.solve()) {
+        print("${x.booleanValue}")
     }
 }
--- clpb/src/test/kotlin/clpb/LabelingTest.kt
+++ clpb/src/test/kotlin/clpb/LabelingTest.kt
@@ -3,6 +3,7 @@ package clpb
 import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.solve.library.toRuntime
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class LabelingTest: BaseTest() {
 
@@ -21,13 +22,13 @@ class LabelingTest: BaseTest() {
 
         termParser.scope.with {
             solution.assertSolutionAssigns(
-                varOf("X") to intOf("1")
+                varOf("X") to intOf(0)
             )
         }
     }
 
     @Test
-    fun testLabelingAnd(){
+    fun testLabelingOr(){
 
         val goal = termParser.parseStruct(
             "sat(X+Y),labeling([X,Y])"
@@ -41,8 +42,65 @@ class LabelingTest: BaseTest() {
 
         termParser.scope.with {
             solution.assertSolutionAssigns(
-                varOf("X") to intOf("0"),
-                varOf("Y") to intOf(1)
+                varOf("X") to intOf(1),
+                varOf("Y") to intOf(0)
+            )
+        }
+    }
+
+    @Test
+    fun testLabelingVarOrBool(){
+
+        val goal = termParser.parseStruct(
+            "sat(X+1),labeling([X])"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("X") to intOf(1)
+            )
+        }
+    }
+
+    @Test
+    fun testLabelingSatFalse(){
+
+        val goal = termParser.parseStruct(
+            "sat(X*0),labeling([X])"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        assertTrue(solution.isNo)
+    }
+
+    @Test
+    fun testLabelingVarNotPreviouslyDefined(){
+
+        val goal = termParser.parseStruct(
+            "sat(X),labeling([X,Y])"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("X") to intOf(0),
+                varOf("Y") to intOf(0)
             )
         }
     }
