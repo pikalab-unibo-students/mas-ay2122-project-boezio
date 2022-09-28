fd06daf13739e537728fb8bef79f8087ec55eead
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-28 16:54:15 +0200

    test(clpb): taut/2 tested

--- clpb/src/test/kotlin/clpb/BaseTest.kt
+++ clpb/src/test/kotlin/clpb/BaseTest.kt
@@ -1,11 +1,9 @@
 package clpb
 
 import it.unibo.tuprolog.core.parsing.TermParser
-import it.unibo.tuprolog.theory.parsing.ClausesParser
 
 abstract class BaseTest {
 
     protected val termParser = TermParser.withDefaultOperators()
-    protected val theoryParser = ClausesParser.withDefaultOperators()
 
 }
---
+++ clpb/src/test/kotlin/clpb/TautTest.kt
@@ -0,0 +1,145 @@
+package clpb
+
+import it.unibo.tuprolog.solve.Solver
+import it.unibo.tuprolog.solve.library.toRuntime
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class TautTest: BaseTest() {
+
+    @Test
+    fun testTautTrueValue(){
+
+        val goal = termParser.parseStruct(
+            "taut(1,T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(1)
+            )
+        }
+    }
+
+    @Test
+    fun testTautFalseValue(){
+
+        val goal = termParser.parseStruct(
+            "taut(0,T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(0)
+            )
+        }
+    }
+
+    @Test
+    fun testTautTrueProposition(){
+
+        val goal = termParser.parseStruct(
+            "taut(1*1,T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(1)
+            )
+        }
+    }
+
+    @Test
+    fun testTautFalseProposition(){
+
+        val goal = termParser.parseStruct(
+            "taut(1*0,T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(0)
+            )
+        }
+    }
+
+    @Test
+    fun testTautVar(){
+
+        val goal = termParser.parseStruct(
+            "taut(X,T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        assertTrue(solution.isNo)
+    }
+
+    @Test
+    fun testTautIsTaut(){
+
+        val goal = termParser.parseStruct(
+            "taut('+'(X,'~'(X)),T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(1)
+            )
+        }
+    }
+
+    @Test
+    fun testTautIsNotTaut(){
+
+        val goal = termParser.parseStruct(
+            "taut('*'(X,'~'(X)),T)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("T") to intOf(0)
+            )
+        }
+    }
+}
