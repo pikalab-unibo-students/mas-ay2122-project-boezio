bc601c3c6cdcd92644acf28e2cf28f4bee7d08bd
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-30 18:38:07 +0200

    test(clpb): tests on weighted_maximum/3

--- clpb/src/test/kotlin/clpb/WeightedMaximumTest.kt
+++ clpb/src/test/kotlin/clpb/WeightedMaximumTest.kt
@@ -3,6 +3,7 @@ package clpb
 import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.solve.library.toRuntime
 import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
 class WeightedMaximumTest: BaseTest() {
 
@@ -34,6 +35,29 @@ class WeightedMaximumTest: BaseTest() {
 
         val goal = termParser.parseStruct(
             "weighted_maximum([1,2,1], [A,B,C], Maximum)"
+        )
+
+        val solver = Solver.prolog.solverWithDefaultBuiltins(
+            otherLibraries = ClpBLibrary.toRuntime()
+        )
+
+        val solution = solver.solveOnce(goal)
+
+        termParser.scope.with {
+            solution.assertSolutionAssigns(
+                varOf("A") to intOf(1),
+                varOf("B") to intOf(1),
+                varOf("C") to intOf(1),
+                varOf("Maximum") to intOf(4),
+            )
+        }
+    }
+
+    @Test @Ignore
+    fun testWeightedMaximumWithLabeling(){
+
+        val goal = termParser.parseStruct(
+            "weighted_maximum([1,2,1], [A,B,C], Maximum), labeling([A,B,C])"
         )
 
         val solver = Solver.prolog.solverWithDefaultBuiltins(
