f8230569f7320c3d2bbeadf60a3c0a51ff4eb9ef
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-09-30 12:42:31 +0200

    style(clpqr): add comment to the example

--- real_variables/src/main/kotlin/ChocoKt/BBInf.kt
+++ real_variables/src/main/kotlin/ChocoKt/BBInf.kt
@@ -11,22 +11,24 @@ fun main() {
     val x = model.realVar("X", -bound, bound, precision)
     val y = model.realVar("Y", -bound, bound, precision)
     val z = model.realVar("Z", -bound, bound, precision)
-    val yInt = model.intVar("xInt", -bound.toInt(), bound.toInt())
-    val zInt = model.intVar("xInt", -bound.toInt(), bound.toInt())
+    val yInt = model.intVar("yInt", -bound.toInt(), bound.toInt())
+    val zInt = model.intVar("zInt", -bound.toInt(), bound.toInt())
 
     // Constraints
     x.ge(y.add(z)).equation().post()
     y.ge(1.0).equation().post()
     z.ge(1.0).equation().post()
+    model.eq(y,yInt).post()
+    model.eq(z,zInt).post()
 
-    // solve as maximization problem
+    // solve as minimization problem
     model.setObjective(Model.MINIMIZE, x)
 
     // solution generation
     val solver = model.solver
 
     while(solver.solve()) {
-        println("$x\n$yInt\n$zInt\n")
+        println("$x\t${yInt.value}\t${zInt.value}\n")
     }
 
 }
